8894ae3db015b3f1c56bfa60d245153000514a12
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2016-07-28 12:57:14 -0300

    Updated script to stress the apache httpd

--- roles/benchmark/files/scripts/stress_test.sh
+++ roles/benchmark/files/scripts/stress_test.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+currentpath=/srv/scripts
+resultspath=$currentpath/results
+totalrequests=5000
+
+# Clean everything
+function prepare_to_run()
+{
+  mkdir -p $resultspath
+  rm -rf $resultspath/*
+}
 
 # Function responsible to stress the application with ab.
 # @param @mpm_type Type of MPM strategy adopted.
@@ -6,9 +18,11 @@ function stress_with_ab()
 {
   local mpm_type=$1
   local uri=$2
-  mkdir -p results
+  prepare_to_run
   for i in {1,10,25,50,100,1000}; do
-    ab -n 9000 -c $i -g "results/gnuplot_"$mpm_type"_"$i".tsv" $uri
+    local plot="$resultspath/gnuplot_"$mpm_type"_"$i".tsv"
+    local csv="$resultspath/csv_"$mpm_type"_"$i".csv"
+    ab -n $totalrequests -c $i -g $plot -e $csv $uri
   done
 }
 
