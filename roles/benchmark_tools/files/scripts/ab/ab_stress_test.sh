9afd7d7672e4104b7f5f078d781daad74305cc3b
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2016-08-18 16:56:33 -0300

    New stress script

--- roles/benchmark_tools/files/scripts/ab/ab_stress_test.sh
+++ roles/benchmark_tools/files/scripts/ab/ab_stress_test.sh
@@ -10,6 +10,7 @@ base="$(dirname "$0")"
 . $base/strategies/varying_requests_and_concurrency.sh --source-only
 . $base/strategies/keep_alive_fixed_requests_varying_concurrency.sh --source-only
 . $base/strategies/increase_request_by_time.sh --source-only
+. $base/strategies/increase_request.sh --source-only
 
 # Clean everything
 function prepare_to_run()
@@ -22,4 +23,5 @@ prepare_to_run
 fixed_requests_varying_concurrency $1
 varying_requests_concurrency $1
 keep_alive_with_fixed_requests_varying_concurrency $1
-increase_request_by_time $1 1000
+increase_request_by_time $1 10000
+increase_request $1 10000
---
+++ roles/benchmark_tools/files/scripts/ab/strategies/increase_request.sh
@@ -0,0 +1,23 @@
+# Function responsible to stress the application with ab.
+# @param @uri Target uri
+
+function increase_request()
+{
+  local uri=$1
+  local increase_by=${2:=200}
+  local results="$resultspath/increase_request"
+
+  mkdir -p $results
+
+  local concurrency=100
+
+  for (( hammered=1; hammered <= 10; hammered++ )); do
+
+    local requests=$(( hammered * increase_by ))
+
+    local plot="$results/$hammered.tsv"
+    local log="$results/$hammered.log"
+    local csv="$results/$hammered.csv"
+    ab -n $requests -c $concurrency -g $plot -e $csv -s 50 $uri > $log 2>&1
+  done
+}
