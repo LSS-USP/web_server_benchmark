1c9781d2ac95f22a22a2e60bb0d9ed74b28eea91
rodrigosiqueira <rodrigosiqueiramelo@gmail.com>
2016-08-18 16:59:07 -0300

    Added R scripts to plot results
    Sigined-off-by: Carlos Eduardo <cems@cemshost.com.br>

--- scripts/generate_graphs.sh
+++ scripts/generate_graphs.sh
@@ -1,81 +1,25 @@
-function set_configuration()
-{
-  read_from=$1
-  image_output=${read_from/tsv/png}
-}
 
-function template_generate()
-{
-  template=$1
-  newfile=$2
-  dir=$3
-  # Replace basic information
-  template=${template/__IMAGE_PATH__/"$newfile"}
-
-  # Replace data paths
-  # ORDER IS IMPORTANT HERE!
-  for folder in {event,prefork,worker}; do
-    # Replace paths in the template
-    current_path=${dir/event/$folder}
-    to_replace="__"${folder^^}"_FILE__"
-    template=${template/$to_replace/$current_path}
-  done
-  echo "$template" > "$newfile.tpl"
-}
-
-function fixed_request_varying_concurrency_csv_templates()
+function increase_request()
 {
   basepath=$1
-  local outputfolder="$basepath/graphs/fixed_request_varying_concurrency_csv_templates"
+  local outputfolder="$basepath/graphs/increase_request"
   mkdir -p $outputfolder
-  i=0
 
+  base="$(dirname "$0")"
+
+  i=0
   # ATTENTION: we iterate on 'event' folder, and below we replace the name
   # this only works because we have the same filenames in all folders
-  for dir in $basepath/event/fixed_requests_varying_concurrency_*.csv; do
+  for dir in $basepath/event/increase_request/*.csv; do
     i=$(( i+= 1 ))
-    local newfile="$outputfolder/response_time_by_request_$i"
-    # Copy template with new name to graphs folder
-    template=$(cat scripts/time_by_served.tpl)
-
-    template_generate "$template" "$newfile" "$dir"
+    local newfile="$outputfolder/response_time_by_request_$i.png"
+    # Replace paths
+    event=${dir/event/event}
+    worker=${dir/event/worker}
+    prefork=${dir/event/prefork}
+    echo "Generating graphs: $i"
+    Rscript --vanilla $base/r_script/binned_data.R $newfile $event $worker $prefork
   done
 }
 
-function fixed_request_varying_concurrency_tsv_templates()
-{
-  basepath=$1
-  local outputfolder="$basepath/graphs/fixed_request_varying_concurrency_tsv_templates"
-  mkdir -p $outputfolder
-  i=0
-
-  # ATTENTION: we iterate on 'event' folder, and below we replace the name
-  # this only works because we have the same filenames in all folders
-  for dir in $basepath/event/fixed_requests_varying_concurrency_*.tsv; do
-    i=$(( i+= 1 ))
-    local newfile="$outputfolder/response_time_by_request_$i"
-    # Copy template with new name to graphs folder
-    template=$(cat scripts/response_time_by_request.tpl)
-
-    template_generate "$template" "$newfile" "$dir"
-  done
-}
-
-function plot_graphs()
-{
-  basepath=$1
-
-  for folders in $basepath/graphs/*; do
-    for files in $folders/*; do
-      imagesfolder=$folders/images
-      mkdir -p $imagesfolder
-      gnuplot $files
-      file=${files/.tpl/.png}
-      mv $file $imagesfolder 2>/dev/null
-    done;
-  done;
-}
-
-fixed_request_varying_concurrency_tsv_templates $1
-fixed_request_varying_concurrency_csv_templates $1
-plot_graphs $1
+increase_request $1
